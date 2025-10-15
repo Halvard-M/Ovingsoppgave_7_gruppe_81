76f2f767653a5d03f9069c3778e53b06d271304f
UnfixedBacon <unfiedbacon@gmail.com>
2025-10-15 16:33:38 +0200

    filer

--- filer.py
+++ filer.py
@@ -8,9 +8,18 @@ filer
 
 import numpy as np
 
-def lagre_til_fil(filer):
-    np.savetxt('fil.csv', , delimiter=';', header='', comments='')
+def lagre_til_fil(emner, studieplan):
 
-def les_fra_fil(filer):
-    data = np.genfromtxt('fil.csv', delimiter=';', missing_values='N/A', skip_header=1)
-    print(data)
+    max_len = max(len(emner),len(studieplan))
+
+    if max_len != 0 and len(emner)<len(studieplan):
+        emner = np.pad(emner, ((0,0),(0, max_len-len(emner))),'constant',constant_values='naa')
+
+    output = np.hstack((emner,studieplan.reshape(-1,1)),dtype=object)
+
+    np.savetxt('output.csv', output, delimiter=',', fmt='%s')
+
+def legre_til_fil(emner, studieplan):
+    input = np.genfromtxt('output.csv', dtype=str, delimiter=',')
+    emner = input[:,[0,1,2]]
+    studieplan = input[:,3]
